2e7d6f44bc2d1137acbd64764fbac72dc9530b7e
devin <devin.kpchumba@student.moringaschool.com>
2025-03-17 16:23:41 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(number1,number2){
+    return  number1 / number2
+}
+
+const square = number => number*number
+
+const add = function(parameter1,parameter2){
+    return parameter1 + parameter2
+}
